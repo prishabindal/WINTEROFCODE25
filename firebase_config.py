3cd33bb618b7c425fb6c1a0713240b6752fc05a5
prishabindal <prishabindal06@gmail.com>
2025-01-06 10:48:34 +0530

    Add files via upload

--- firebase_config.py
+++ firebase_config.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials
 # Initialize Firebase Admin SDK
 def initialize_firebase():
     #Initializing DB
-    cred = credentials.Certificate("db_encrypted_key.json")
+    cred = credentials.Certificate("voting_system_key.json")
     firebase_admin.initialize_app(cred)
     print("Firebase initialized")
 
